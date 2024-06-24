54a4c770e14014eff5fd7dd5f49e7837710c0c0b
William Wong <williamw205@gmail.com>
2024-06-23 17:28:17 -0700

    Added in logo/images and created the header, hero, and footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,170 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div className="w-full bg-white">
+      <header className="flex justify-between items-center py-4 px-14">
+        <div className="flex items-center gap-2">
+          <img
+            src="/Logo.svg"
+            alt="Like Music Logo"
+            width={32}
+            height={32}
+            className="w-8 h-8"
+          />
+          <span className="text-lg font-semibold text-black">PlaylistGPT </span>
         </div>
-      </div>
-
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+        <div className="flex items-center gap-4">
+          <Link
+            href="#"
+            className="text-black hover:text-gray-700"
+            prefetch={false}
+          >
+            Home
+          </Link>
+          <Link
+            href="#"
+            className="inline-flex h-10 items-center justify-center rounded-md bg-black px-6 text-sm font-medium text-white shadow transition-colors hover:bg-gray-800 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50"
+            prefetch={false}
+          >
+            Generate Playlist
+          </Link>
+        </div>
+      </header>
+      <header className="flex flex-col items-center justify-center gap-5 py-20">
+        <h1 className="text-6xl font-extrabold mx-auto text-black text-center">
+          Generate your next Playlist
+          <br />
+          with AI in seconds
+        </h1>
+        <p className="text-lg text-gray-700 text-center">
+          PlaylistGPT simplifies the process for you to create playlists in{" "}
+          <br />
+          seconds, completely for free.
+        </p>
+        <div className="flex items-center gap-2">
+          <Link
+            href="#"
+            className="inline-flex h-12 items-center justify-center rounded-md bg-black px-8 text-sm font-medium text-white shadow transition-colors hover:bg-gray-800 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50"
+            prefetch={false}
+          >
+            Generate Playlist
+          </Link>
+        </div>
+      </header>
+      <section className="container mx-auto grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-2 py-12">
+        <div className="flex flex-col items-center gap-2">
+          <img
+            src="/Paradise-Album.svg"
+            alt="Album Cover"
+            width={250}
+            height={250}
+            className="rounded-lg"
+          />
+          <div className="text-center">
+            <h3 className="text-black font-semibold">Upbeat Pop Playlist</h3>
+            <p className="text-gray-500">Ariana Grande, Dua Lipa</p>
+          </div>
+        </div>
+        <div className="flex flex-col items-center gap-2">
+          <img
+            src="/Paradise-Album.svg"
+            alt="Album Cover"
+            width={250}
+            height={250}
+            className="rounded-lg"
+          />
+          <div className="text-center">
+            <h3 className="text-black font-semibold">Chill Indie Playlist</h3>
+            <p className="text-gray-500">Bon Iver, Phoebe Bridgers</p>
+          </div>
+        </div>
+        <div className="flex flex-col items-center gap-2">
+          <img
+            src="/Paradise-Album.svg"
+            alt="Album Cover"
+            width={250}
+            height={250}
+            className="rounded-lg"
+          />
+          <div className="text-center">
+            <h3 className="text-black font-semibold">Workout Beats Playlist</h3>
+            <p className="text-gray-500">Kendrick Lamar, Megan Thee Stallion</p>
+          </div>
+        </div>
+        <div className="flex flex-col items-center gap-2">
+          <img
+            src="/Paradise-Album.svg"
+            alt="Album Cover"
+            width={250}
+            height={250}
+            className="rounded-lg"
+          />
+          <div className="text-center">
+            <h3 className="text-black font-semibold">Retro Hits Playlist</h3>
+            <p className="text-gray-500">Queen, The Beatles</p>
+          </div>
+        </div>
+        <div className="flex flex-col items-center gap-2">
+          <img
+            src="/Paradise-Album.svg"
+            alt="Album Cover"
+            width={250}
+            height={250}
+            className="rounded-lg"
+          />
+          <div className="text-center">
+            <h3 className="text-black font-semibold">
+              Rap Essentials Playlist
+            </h3>
+            <p className="text-gray-500">Kanye West, Nas</p>
+          </div>
+        </div>
+        <div className="flex flex-col items-center gap-2">
+          <img
+            src="/Paradise-Album.svg"
+            alt="Album Cover"
+            width={250}
+            height={250}
+            className="rounded-lg"
+          />
+          <div className="text-center">
+            <h3 className="text-black font-semibold">
+              Country Classics Playlist
+            </h3>
+            <p className="text-gray-500">Johnny Cash, Dolly Parton</p>
+          </div>
+        </div>
+      </section>
+      <footer className="bg-white py-8 border-t">
+        <div className="container flex justify-between items-center">
+          <p className="text-gray-500">Created by William.</p>
+          <nav className="flex gap-4">
+            <Link
+              href="#"
+              className="text-gray-500 hover:text-gray-700"
+              prefetch={false}
+            >
+              Privacy
+            </Link>
+            <Link
+              href="#"
+              className="text-gray-500 hover:text-gray-700"
+              prefetch={false}
+            >
+              Terms
+            </Link>
+            <Link
+              href="#"
+              className="text-gray-500 hover:text-gray-700"
+              prefetch={false}
+            >
+              Contact
+            </Link>
+          </nav>
+        </div>
+      </footer>
+    </div>
   );
 }
